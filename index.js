d9bc466148c678dea7d453e301b99299082dd425
Asif <asyfsaeed@gmail.com>
2020-04-27 22:36:40 +0500

    jimping testing

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+ ** lambda-jimp-watermark-s3 -- Nodejs service for AWS Lambda to apply watermark to images stored on S3
+ ** Copyright (c) 2020 Asif Saeed <asyfsaeed@gmail.com>
+ */
+
+(function () {
+  'use strict';
+
+  module.exports = require('./lib/lambdaJimpWrapper');
+
+})();
